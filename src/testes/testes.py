e0a7b2bc7afd85aaf45ac0875b657800bfbc1d6b
AugustoP-C <piresaugusto046@gmail.com>
2023-03-29 15:39:57 -0300

    segundo commit

--- src/testes/testes.py
+++ src/testes/testes.py
@@ -9,17 +9,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-nav = webdriver.Chrome()
-nav.get("https://fast.com/pt/")
-wait = WebDriverWait(nav, 60)
-wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="show-more-details-link"]'))).click()
-time.sleep(2)
-dowl = nav.find_element("xpath", '//*[@id="speed-value"]').text
-time.sleep(40)
-upl = nav.find_element("xpath", '//*[@id="upload-value"]').text
-lateNC = nav.find_element("xpath", '//*[@id="latency-value"]').text
-lateC = nav.find_element("xpath", '//*[@id="bufferbloat-value"]').text
-print("A rede que vc esta conectada tem:\n{0} de Dowload\n{1} de Upload\n{2} de Latencia carregada\n{3} de Latencia não carregada".format(dowl, upl, lateC, lateNC))
+def fast():
+    nav = webdriver.Chrome()
+    nav.get("https://fast.com/pt/")
+    wait = WebDriverWait(nav, 60)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="show-more-details-link"]'))).click()
+    time.sleep(2)
+    dowl = nav.find_element("xpath", '//*[@id="speed-value"]').text
+    time.sleep(40)
+    upl = nav.find_element("xpath", '//*[@id="upload-value"]').text
+    lateNC = nav.find_element("xpath", '//*[@id="latency-value"]').text
+    lateC = nav.find_element("xpath", '//*[@id="bufferbloat-value"]').text
+    print("A rede que vc esta conectada tem:\n{0} de Dowload\n{1} de Upload\n{2} de Latencia carregada\n{3} de Latencia não carregada".format(dowl, upl, lateC, lateNC))
+fast()
 
 # i = int(input("Ate qual numero vc quer que eu conte?\n"))
 # n = int(0)
