bd0e71b82b21eecd3a164b6d3baec67d3a37a1bb
AugustoP-C <piresaugusto046@gmail.com>
2023-05-30 17:52:31 -0300

    renomeando projetos

--- src/tt.py
+++ src/tt.py
@@ -14,18 +14,18 @@ import re
 # tt = re.sub(r'[\n]', '', q)
 # print(tt)
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-nav = webdriver.Chrome()
-nav.get("https://www.transparencia.mg.gov.br/compras-e-patrimonio/compras-e-contratos/comprasecontratos-resultado-pesquisa-avancada")
-nav.find_element(By.XPATH, '//*[@id="jform_TEXTO_CONTRATADO"]').send_keys("positivo tecnologia")
-nav.find_element(By.XPATH, '//*[@id="pesquisarcontratado"]').click()
-time.sleep(1)
-nav.find_element(By.XPATH, '//*[@id="dialog"]/table/tbody/tr[1]/td/a').click()
-nav.find_element(By.XPATH, '/html/body/div[4]/div[1]/button').click()
-time.sleep(1)
-nav.find_element(By.XPATH, '//*[@id="btn_pesquisar"]').click()
-dropdown_propt = str(nav.current_url).split("/")
-propt = dropdown_propt[27]
-print(propt)
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+# nav = webdriver.Chrome()
+# nav.get("https://www.transparencia.mg.gov.br/compras-e-patrimonio/compras-e-contratos/comprasecontratos-resultado-pesquisa-avancada")
+# nav.find_element(By.XPATH, '//*[@id="jform_TEXTO_CONTRATADO"]').send_keys("positivo tecnologia")
+# nav.find_element(By.XPATH, '//*[@id="pesquisarcontratado"]').click()
+# time.sleep(1)
+# nav.find_element(By.XPATH, '//*[@id="dialog"]/table/tbody/tr[1]/td/a').click()
+# nav.find_element(By.XPATH, '/html/body/div[4]/div[1]/button').click()
+# time.sleep(1)
+# nav.find_element(By.XPATH, '//*[@id="btn_pesquisar"]').click()
+# dropdown_propt = str(nav.current_url).split("/")
+# propt = dropdown_propt[27]
+# print(propt)
